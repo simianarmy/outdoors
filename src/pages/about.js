6ffa2930962ef308c0c3e2b25425e015db2b1bf5
Marc Mauger <simianarmy@gmail.com>
2020-02-12 17:25:34 -0800

    added quote to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,5 +5,11 @@ export default () => (
   <Layout>
     <h1>About Me</h1>
     <p>A dude walking and sometimes climbing</p>
+    <p>
+    <blockquote>Climb the mountains and get their good tidings. Nature’s peace will
+      flow into you as sunshine flows in to trees. The winds will blow their own freshness into you,
+      and the storms their energy, while cares will drop off like autumn leaves. - John Muir
+    </blockquote>
+  </p>
   </Layout>
 )
