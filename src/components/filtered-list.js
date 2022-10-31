7f394788fbd39ac9c4630ea98ee5d578a4c6f0fd
MarcMaugerZwift <marc-mauger@zwift.com>
2022-10-31 13:50:18 -0700

    prettiered a file

--- src/components/filtered-list.js
+++ src/components/filtered-list.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { displayDate } from '../utils/dates'
+import React from 'react';
+import { Link } from 'gatsby';
+import { displayDate } from '../utils/dates';
 
 function FilteredList({ items }) {
   return items.map(({ node }) => (
     <div key={node.id} className="mb-4">
-      <Link
-        to={node.fields.slug}
-      >
+      <Link to={node.fields.slug}>
         <h3 className="text-lg font-bold leading-snug">
           {node.frontmatter.title}
         </h3>
@@ -17,7 +15,7 @@ function FilteredList({ items }) {
         <p className="text-sm">{node.excerpt}</p>
       </Link>
     </div>
-  ))
+  ));
 }
 
-export default FilteredList
+export default FilteredList;
