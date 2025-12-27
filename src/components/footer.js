04b289ccefc96a097b77f68f7771e70e73808e6d
Marc Mauger <simianarmy@gmail.com>
2025-12-27 10:34:03 -0500

    Version footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import pacakgeInfo from '../../package.json';
 
 const ListLink = (props) => (
   <li className="inline-block mr-4">
@@ -10,11 +11,16 @@ const ListLink = (props) => (
 );
 
 const Footer = () => (
-  <div className="h-16 bg-slate-400 flex text-white justify-end items-center">
-    <ul>
-      <ListLink to="/about/">About</ListLink>
-      <ListLink to="/contact/">Contact</ListLink>
-    </ul>
+  <div className="h-16 bg-slate-400 flex justify-between w-full text-white items-center">
+    <div className="ml-4 mr-4">
+      <span className="text-sm">&copy; Simian Labs v{pacakgeInfo.version}</span>
+    </div>
+    <div className="justify-self-end">
+      <ul>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </div>
   </div>
 );
 
